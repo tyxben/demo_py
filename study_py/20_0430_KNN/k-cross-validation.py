2c27756235d4a0b6f171ed55ce1d842778e6ee29
weitianyuan <weitianyuan521>
2020-04-30 20:35:27 +0800

    KNN cross

--- study_py/20_0430_KNN/k-cross-validation.py
+++ study_py/20_0430_KNN/k-cross-validation.py
@@ -6,4 +6,16 @@
 # @File    : k-cross-validation.py
 # @Software: PyCharm
 
-#
+from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import KFold
+from sklearn.model_selection import GridSearchCV
+
+iris = datasets.load_iris()
+X, y = iris.data, iris.target
+
+paramers = {'n_neighbors': [1, 3, 5, 7, 9, 11, 13, 15]}
+knn = KNeighborsClassifier()
+clf = GridSearchCV(knn, paramers, cv=5)
+clf.fit(X, y)
+print("best_score%.2f" % clf.best_score_, "best_k:", clf.best_params_)
